c41407ff926c22ad916594d786f137e6efef8c58
Simon Jentsch <simon.jentsch@deliveryhero.com>
2020-02-09 21:46:18 +0100

    Remove wrong ghost button variants

--- src/components/Breadcrumb.bojagi.js
+++ src/components/Breadcrumb.bojagi.js
@@ -10,20 +10,20 @@ export const Basic = () =>
   <Breadcrumb>
     <Breadcrumb.Item>Home</Breadcrumb.Item>
     <Breadcrumb.Item>
-      <a href="">Application Center</a>
+      <a href="#">Application Center</a>
     </Breadcrumb.Item>
     <Breadcrumb.Item>
-      <a href="">Application List</a>
+      <a href="#">Application List</a>
     </Breadcrumb.Item>
     <Breadcrumb.Item>An Application</Breadcrumb.Item>
   </Breadcrumb>
 
 export const BreadcrumbWithIcon = () => 
   <Breadcrumb>
-    <Breadcrumb.Item href="">
+    <Breadcrumb.Item href="#">
       <Icon type="home" />
     </Breadcrumb.Item>
-    <Breadcrumb.Item href="">
+    <Breadcrumb.Item href="#">
       <Icon type="user" />
       <span>Application List</span>
     </Breadcrumb.Item>
--- src/components/GhostButton.bojagi.js
+++ src/components/GhostButton.bojagi.js
@@ -5,27 +5,12 @@ export default {
   title: 'GhostButton',
 };
 
-export const buttonDefault = () => 
-  <Button>
-    Default
-  </Button>
-
 export const buttonPrimary = () => 
   <Button type="primary">
     Primary
-  </Button>
-
-export const buttonDashed = () =>
-  <Button type="dashed">
-    Dashed Button
   </Button>
 
 export const buttonDanger = () =>
   <Button type="danger">
     Danger, danger. High Voltage!
   </Button>
-
-export const linkButton = () =>
-  <Button type="link">
-    Link me!
-  </Button> 

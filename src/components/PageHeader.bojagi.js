b2f845b4b405089fa21a092dd0b046ae5a68da16
Simon Jentsch <simon.jentsch@deliveryhero.com>
2020-02-10 00:30:48 +0100

    Add Wrapper to PageHeader

--- src/components/PageHeader.bojagi.js
+++ src/components/PageHeader.bojagi.js
@@ -20,23 +20,28 @@ const routes = [
   },
 ];
 
-export const Simple = () =>
-  <PageHeader
-    style={{
-      border: '1px solid rgb(235, 237, 240)',
-    }}
-    onBack={() => null}
-    title="Title"
-    subTitle="This is a subtitle"
-  />
+const Wrapper = ({children}) => <div style={{flexGrow: 1}}>{children}</div>;
 
-  export const WithBreadcrumb = () =>
+export const Simple = () =>
+  <Wrapper>
     <PageHeader
       style={{
         border: '1px solid rgb(235, 237, 240)',
       }}
+      onBack={() => null}
       title="Title"
-      breadcrumb={{ routes }}
       subTitle="This is a subtitle"
     />
+  </Wrapper>
 
+  export const WithBreadcrumb = () =>
+    <Wrapper>
+      <PageHeader
+        style={{
+          border: '1px solid rgb(235, 237, 240)',
+        }}
+        title="Title"
+        breadcrumb={{ routes }}
+        subTitle="This is a subtitle"
+      />
+    </Wrapper>
